dda4864841f2fcfaccf10c6cfbe1b9da31a733c6
Duan Zhiyan <dzy0451@gmail.com>
2014-05-08 12:00:22 +0800

    logo size issue fix

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
 <?php
 $lean_logo = get_header_image();
-    $lean_logo = $lean_logo
-    ? ('<img src="'. esc_url($lean_logo). '" />' )
-    : get_bloginfo('name');
+$lean_logo_style = $lean_logo ? '' : 'style="line-height:80px;"';
+$lean_logo = $lean_logo
+? ('<img src="'. esc_url($lean_logo). '" />' )
+: get_bloginfo('name');
 ?>
 
 <div id="sidebar">
-	<a id="logo" href="<?php echo esc_url(home_url());?>" title="<?php bloginfo('name');?>"><?php echo $lean_logo; ?></a>
+    <a id="logo" href="<?php echo esc_url(home_url());?>" title="<?php bloginfo('name');?>" <?php echo $lean_logo_style ?>><?php echo $lean_logo; ?></a>
 	<?php
 
 	$defaults = array(
